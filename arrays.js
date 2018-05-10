ad3cfd8cb85d0f39aadf12267246545ca52b461a
dave.w.burns@gmail.com <dave.w.burns@gmail.com>
2018-05-10 17:10:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, elememt) {
+  array = [...array, elememt];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element) 
   return array;
 }
+  

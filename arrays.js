229393b930e8f5feb61fa0deac9a856df57cd27b
dave.w.burns@gmail.com <dave.w.burns@gmail.com>
2018-05-10 18:05:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.shift(element)
+  array.push(element)
   return array;
 }
 

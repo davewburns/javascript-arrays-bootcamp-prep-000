19ca9b458525be99077ec943158610341cc8a628
dave.w.burns@gmail.com <dave.w.burns@gmail.com>
2018-05-10 18:10:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -43,6 +43,8 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array;
 }
  function removeElementFromEndOfArray(array) {
-   array.slice(0, array.length -1)
-   return array;
+    remAray = array.slice(0, array.length -1)
+   //return array;
+   return remArray;
  }
+ 
